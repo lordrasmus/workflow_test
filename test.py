72ea3b140c3ce319171b81faba41f82830418a1a
Ramin Moussavi <lordrasmus@gmail.com>
2023-11-18 10:29:33 +0100

    test

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python
 
 import os
+import hashlib
+
+
+hash_object = hashlib.sha256()
+hash_object.update(os.environ["UPLOAD_KEY"].encode('utf-8'))
+hash_value = hash_object.hexdigest()
 
 print( os.environ["UPLOAD_KEY"] )
+print( hash_value )
